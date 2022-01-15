5eee25aaf041e41f0d3d5f7356f73adba80d1dd5
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2022-01-14 21:08:46 -0500

    states can be selected; highlight selected state in blue

--- caf/src/components/nfadiagram.js
+++ caf/src/components/nfadiagram.js
@@ -13,15 +13,38 @@ const Nfadiagram = (props) => {
       F: [],
       stateRadius: 30
     }
-  )
+  );
+  const [selected, updateSelected] = useState();
+
+  const selectNear = (x, y) => {
+    let nearest = selected;
+    machine.Q.forEach( (value, key) => {
+      if (Math.sqrt(Math.pow(value[0] - x, 2) + Math.pow(value[1] - y, 2)) < machine.stateRadius){
+        nearest = key;
+      }
+    })
+    if (nearest == selected) {
+      updateSelected("");
+    }
+    else {
+      updateSelected(nearest);
+    }
+  }
 
   const draw = (ctx) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-    ctx.fillStyle = '#000000'
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     machine.Q.forEach((value, key) => {
         ctx.beginPath();
         ctx.arc(value[0], value[1], machine.stateRadius, 0, Math.PI * 2, true);
-        ctx.stroke();
+        if (key == selected){
+          ctx.fillStyle = '#99AAFF'; // Color of selected state
+          ctx.fill();
+          ctx.fillStyle = '#111188';
+        }
+        else{
+          ctx.fillStyle = '#000000';
+          ctx.stroke();
+        }
         ctx.moveTo(value[0] - machine.stateRadius, value[1] - machine.stateRadius);
         ctx.textAlign = "center";
         ctx.font = (machine.stateRadius) + "px serif";
@@ -30,14 +53,16 @@ const Nfadiagram = (props) => {
   }
 
   const handleClick = (e) => {
+    let canvX = e.clientX - e.target.offsetLeft;  // X position on canvas of click
+    let canvY = e.clientY - e.target.offsetTop;   // Y position on canvas of click
     switch (e.detail) {
       case 1:
-        console.log("click");
+        selectNear(canvX, canvY);
+        console.log(e);
         break;
       case 2:
-        console.log(e);
-        machine.Q.set("q" + machine.Q.size, [e.clientX - e.target.offsetLeft, e.clientY - e.target.offsetTop]);
-        console.log(machine);
+        // Create a new state at the location of the double click
+        machine.Q.set("q" + machine.Q.size, [canvX, canvY]);
         break;
       case 3:
         console.log("triple click");
